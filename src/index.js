91190b8dc87deb78fc7e679d923c440e8e916146
Maurik <Ferry.van.Maurik@nike.com>
2017-04-27 10:21:45 +0200

    ReactDOM vs React

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Create new component. This component should produce some HTML
 
@@ -8,4 +9,4 @@ const App = function() {
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 
-React.React(App);
+ReactDOM.React(App);
